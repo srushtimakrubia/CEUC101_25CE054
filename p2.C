7234c59f423cb5b5bde3b2824046246b6b17afa3
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-07-28 15:30:25 +0530

    Add files via upload

---
+++ p2.C
@@ -0,0 +1,17 @@
+//aim : program to find out area and perimeter of the rectangular plot
+//programer : srushti makrubiya
+//last updated date : 18/07/25
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int l,b,a,p;
+   clrscr();
+   l=70;
+   b=90;
+   a=l*b;
+   p=2*(l+b);
+   printf("\narea=%d",a);
+   printf("\nperimeter=%d",p);
+   getch();
+}
