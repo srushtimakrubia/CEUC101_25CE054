5502147640518330cefaf191b26e6dc22f1a3a83
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-07-28 15:32:12 +0530

    Add files via upload

---
+++ p3.C
@@ -0,0 +1,18 @@
+
+/*aim:find bmi
+editor:srushti makrubiya
+date:21/07/2025*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float h,w,bmi;
+	clrscr();
+	printf("enter height in meter");
+	scanf("%f",&h);
+	printf("enter weight in kilogram");
+	scanf("%f",&w);
+	bmi=w/(h*h);
+	printf("bmi=%f",bmi);
+	getch();
+}
