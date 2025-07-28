45b7520845ee90abdba38aea60ab9f2189cd2446
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-07-28 14:57:02 +0530

    Add files via upload

---
+++ P8.C
@@ -0,0 +1,16 @@
+/*aim:cheking eligibilyti
+programer:srushti makrubiya
+updated date : 28/07/2025*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int age;
+	clrscr();
+	printf("enter your agr");
+	scanf("%d",&age);
+	age>=18?printf("you are eligible for opning account"):printf("you are not eligible for opening account");
+	getch();
+
+
+}
