7a411c679fe352d745cbd744bc98106e8ff5b4fd
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-07-29 23:19:36 +0530

    Add files via upload

---
+++ P7.C
@@ -0,0 +1,14 @@
+/*aim:entry fees
+programmer : srushti makrubiya
+updated date : 29/07/2025*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{       int age;
+	clrscr();
+	printf("enter your age");
+	scanf("%d",&age);
+	age>=12?printf("pay 100 rupees"):printf("free entry");
+	getch();
+
+}
