6aebdf27db1335952cd5593923e08811438f9b22
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-08-01 14:26:34 +0530

    Add files via upload

---
+++ p1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\"name:Yug Suthar\"");
+printf("Department:U and P patel department of CE\n");
+printf("Institute:Chandubhai S Patel institute of technology\n");
+printf("university:Charotar university of science and technology\n");
+printf("faculty:faculty of technology and enginering\n");
+printf("address:C/5 siddharth bunglows sama savli road vadodara\n");
+getch();
+}
