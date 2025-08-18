acd0970360cc8977e2013518f9607b444fe86c7a
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-08-18 16:18:09 +0530

    Add files via upload

---
+++ P13.C
@@ -0,0 +1,16 @@
+/*aim : smart water system company
+programer : srushti makrubiya
+date : 04.08.2025*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i;
+	clrscr();
+	for(i=10;i<=100;i=i+10)
+	{
+			printf("\ncurrent water level : %d",i);
+	}
+	printf("\ntank is full");
+	getch();
+}
