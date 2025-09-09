7975c6f68bba057170d45dbd0d78495346b18929
srushtimakrubia <srushtimakrubiya@gmail.com>
2025-09-09 18:04:02 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+    printf("\nint size : %d",sizeof(int));
+    printf("\nfloat size : %d",sizeof(float));
+    printf("\ndouble size : %d",sizeof(double));
+    printf("\nchar size : %d",sizeof(char));
+}
